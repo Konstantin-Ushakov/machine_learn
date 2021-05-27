de2ce6b630d5998dee2693d84d2bf66ff6b55723
Konstantin Ushakov <ushk2010ushk@gmail.com>
2021-05-27 22:32:35 +0600

    Add sidebar for neural network and title

--- modules/9_streamlit.py
+++ modules/9_streamlit.py
@@ -5,7 +5,7 @@
 # 
 # https://test-ml-1.herokuapp.com/
 
-# In[ ]:
+# In[2]:
 
 
 import pandas as pd
@@ -20,12 +20,13 @@ import tensorflow
 from tensorflow import keras
 
 
-# In[7]:
+# In[3]:
 
 
 from keras.layers import Dense, InputLayer
 from keras.models import Sequential
 from keras.activations import *
+from keras.callbacks import Callback
 
 
 # # Предобработка данных
@@ -115,15 +116,38 @@ def init_model(model="", parameters={}):
     return regressor
 
 
+# [Keras callback](https://keras.io/guides/writing_your_own_callbacks/)
+
+# In[7]:
+
+
+class CustomCallback(Callback):
+    def __init__(self, epochs=1):
+        super(CustomCallback, self).__init__()
+        self.epochs = epochs
+        self.pr_bar = st.progress(0)
+        pass
+    
+    def on_epoch_end(self, epoch, logs=None):
+        keys = list(logs.keys())
+        if epoch < self.epochs:
+            progress = float(epoch) / self.epochs
+        else:
+            progress = 100
+        self.pr_bar.progress(progress)
+        pass
+
+
 # Метод <code>trainee_model</code> обучает модель с гиперпараметрами
 
-# In[29]:
+# In[6]:
 
 
 def trainee_model(X_train, y_train, model="", parameters={'epochs':1}):
     regressor = init_model(model, parameters)
     if model == 'neural_network':
-        regressor.fit(X_train, y_train, epochs=parameters['epochs'])
+        regressor.fit(X_train, y_train, epochs=parameters['epochs'],
+                     callbacks=[CustomCallback(parameters['epochs'])])
     else:
         regressor.fit(X_train, y_train)
     return regressor
@@ -179,6 +203,16 @@ def deserialize_model_file(filename="ser.dat"):
 
 # Загрузка данных из файла
 
+# In[9]:
+
+
+@st.cache
+def load_description(filename='../data/automobile_description.md'):
+    with open(filename, 'r', encoding="utf-8") as fin:
+        descr_auto = fin.read()
+        return descr_auto
+
+
 # In[53]:
 
 
@@ -193,7 +227,7 @@ def load_data(filename='../data/automobile_preprocessed.csv'):
 
 def main():
     page = st.sidebar.selectbox('Choose a page', ['Description', 'Regressor'])
-    
+    df = load_data('../data/automobile_preprocessed.csv')
     if page == 'Description':
         st.header('Automobile dataset description')
         description_task = '''## Task 3
@@ -206,106 +240,11 @@ def main():
 
 Найдите реализации методов в sklearn, оставьте в нотбуке ссылки на документацию. Найдите наилучшие гиперпараметры. Оцените качество моделей: R2, Mean Square Error(MSE), Root Mean Square Error(RMSE),  mean absolute error (MAE). Свои действия снабжайте пояснениями.'''
         st.write(description_task)
-        description = '''
-        ## Автомобили [Источник](https://archive.ics.uci.edu/ml/datasets/Automobile)
-
-### Описание данных
-
-     Attribute:                Attribute Range:
-     ------------------        -----------------------------------------------
-  1. symboling:                -3, -2, -1, 0, 1, 2, 3.
-  2. normalized-losses:        continuous from 65 to 256.
-  3. make:                     alfa-romero, audi, bmw, chevrolet, dodge, honda,
-                               isuzu, jaguar, mazda, mercedes-benz, mercury,
-                               mitsubishi, nissan, peugot, plymouth, porsche,
-                               renault, saab, subaru, toyota, volkswagen, volvo
-  4. fuel-type:                diesel, gas.
-  5. aspiration:               std, turbo.
-  6. num-of-doors:             four, two.
-  7. body-style:               hardtop, wagon, sedan, hatchback, convertible.
-  8. drive-wheels:             4wd, fwd, rwd.
-  9. engine-location:          front, rear.
- 10. wheel-base:               continuous from 86.6 120.9.
- 11. length:                   continuous from 141.1 to 208.1.
- 12. width:                    continuous from 60.3 to 72.3.
- 13. height:                   continuous from 47.8 to 59.8.
- 14. curb-weight:              continuous from 1488 to 4066.
- 15. engine-type:              dohc, dohcv, l, ohc, ohcf, ohcv, rotor.
- 16. num-of-cylinders:         eight, five, four, six, three, twelve, two.
- 17. engine-size:              continuous from 61 to 326.
- 18. fuel-system:              1bbl, 2bbl, 4bbl, idi, mfi, mpfi, spdi, spfi.
- 19. bore:                     continuous from 2.54 to 3.94.
- 20. stroke:                   continuous from 2.07 to 4.17.
- 21. compression-ratio:        continuous from 7 to 23.
- 22. horsepower:               continuous from 48 to 288.
- 23. peak-rpm:                 continuous from 4150 to 6600.
- 24. city-mpg:                 continuous from 13 to 49.
- 25. highway-mpg:              continuous from 16 to 54.
- 26. price:                    continuous from 5118 to 45400.
-
-8. Missing Attribute Values: (denoted by "?")
-   Attribute #:   Number of instances missing a value:
-   2.             41
-   6.             2
-   19.            4
-   20.            4
-   22.            2
-   23.            2
-   26.            4
-
-### Source Information
-   -- Creator/Donor: Jeffrey C. Schlimmer (Jeffrey.Schlimmer@a.gp.cs.cmu.edu)
-   -- Date: 19 May 1987
-   -- Sources:
-     1) 1985 Model Import Car and Truck Specifications, 1985 Ward's
-        Automotive Yearbook.
-     2) Personal Auto Manuals, Insurance Services Office, 160 Water
-        Street, New York, NY 10038 
-     3) Insurance Collision Report, Insurance Institute for Highway
-        Safety, Watergate 600, Washington, DC 20037
-
-### Past Usage
-   -- Kibler,~D., Aha,~D.~W., \& Albert,~M. (1989).  Instance-based prediction
-      of real-valued attributes.  {\it Computational Intelligence}, {\it 5},
-      51--57.
-	 -- Predicted price of car using all numeric and Boolean attributes
-	 -- Method: an instance-based learning (IBL) algorithm derived from a
-	    localized k-nearest neighbor algorithm.  Compared with a
-	    linear regression prediction...so all instances
-	    with missing attribute values were discarded.  This resulted with
-	    a training set of 159 instances, which was also used as a test
-	    set (minus the actual instance during testing).
-	 -- Results: Percent Average Deviation Error of Prediction from Actual
-	    -- 11.84% for the IBL algorithm
-	    -- 14.12% for the resulting linear regression equation
-
-4. Relevant Information:
-   -- Description
-      This data set consists of three types of entities: (a) the
-      specification of an auto in terms of various characteristics, (b)
-      its assigned insurance risk rating, (c) its normalized losses in use
-      as compared to other cars.  The second rating corresponds to the
-      degree to which the auto is more risky than its price indicates.
-      Cars are initially assigned a risk factor symbol associated with its
-      price.   Then, if it is more risky (or less), this symbol is
-      adjusted by moving it up (or down) the scale.  Actuarians call this
-      process "symboling".  A value of +3 indicates that the auto is
-      risky, -3 that it is probably pretty safe.
-
-      The third factor is the relative average loss payment per insured
-      vehicle year.  This value is normalized for all autos within a
-      particular size classification (two-door small, station wagons,
-      sports/speciality, etc...), and represents the average loss per car
-      per year.
-
-   -- Note: Several of the attributes in the database could be used as a
-            "class" attribute.
-
-        '''
+        st.dataframe(df)
+        description = load_description()
         st.write(description)
     else:
         st.header('Automobile dataset')
-        df = load_data('../data/automobile_preprocessed.csv')
         # Set model type
         model_type = st.selectbox('Select model', ['elastic_net', 'ridge', 'sgd_regressor', 'neural_network'])
         parameters = {}
@@ -346,7 +285,7 @@ def main():
             parameters['optimizer'] = optimizer
             loss = st.selectbox('Select loss function', ['mse', 'mae'], key='loss')
             parameters['loss'] = loss
-            epochs = st.slider('Select number of epochs', min_value=1, max_value=1000, value=1, step=3, key='epochs')
+            epochs = st.slider('Select number of epochs', min_value=1, max_value=2000, value=1, step=6, key='epochs')
             parameters['epochs'] = epochs
 
         X_train, y_train, X_test, y_test = prepared_dataset(df)
